534a92fd1a93603fa67a52ba8b042246a27127b0
Babaian Ihor <ukr.nox@gmail.com>
2021-07-28 16:45:19 +0300

    Added readme.md and some files from HW1;

---
+++ JELesson_1_GIT_ Introduction/hometask_1.sh
@@ -0,0 +1,19 @@
+git init
+>readme.md
+git add readme.md
+git commit -m "(<HOMETASK_1>) Added first file readme.md"
+git branch hometask_1
+git branch
+git checkout hometask_1
+>a.txt
+git add a.txt
+git commit -m "(<HOMETASK-01>) Added file a.txt"
+>b.txt
+git add b.txt
+git commit -m "(<HOMETASK-01>) Added file b.txt"
+>c.txt
+git add c.txt
+git commit -m "(<HOMETASK-01>) Added file c.txt"
+git remote add origin https://github.com/Ukrnox/hillelJE.git
+git push -u origin hometask_1
+gitk --all
